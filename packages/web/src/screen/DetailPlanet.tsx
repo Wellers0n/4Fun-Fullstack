fb438915ae16a493fb1f81b943117e98404be330
Wellerson <wellerson.coffee@gmail.com>
2019-06-21 00:43:11 -0300

    add button detail planets

--- packages/web/src/screen/DetailPlanet.tsx
+++ packages/web/src/screen/DetailPlanet.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { graphql, QueryRenderer, createFragmentContainer } from "react-relay";
-// import styled from "styled-components";
-import Environment from "./../relay/environment";
+import styled from "styled-components";
 import createQueryRenderer from "./../relay/createQueryRenderer";
 import { DetailPlanet_planet } from "./__generated__/DetailPlanet_planet.graphql";
 import { RouteComponentProps } from "react-router";
+import { Button } from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
 
 type Props = {
   planet: DetailPlanet_planet;
@@ -15,16 +16,38 @@ type PropsRouter = {
   id: string;
 };
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  display: flex;
+  background: #272425;
+  height: 60px;
+`;
+
+const ButtonMain = styled(Button)`
+  &&{
+    // background:red
+  }
+`
+
 const DetailPlanet = ({ history, planet }: Props) => {
   return (
-    <div>
+    <Container>
+      <Header>
+        <ButtonMain onClick={() => history.push("/home")}>
+          <ArrowBack />
+        </ButtonMain>
+      </Header>
       <div>
         <img src={planet.img} alt="planet" width={200} height={200} />
       </div>
       <h1>{planet.name}</h1>
       <div>{planet.description}</div>
-      <button onClick={() => history.push("/home")}>back</button>
-    </div>
+      <button >back</button>
+    </Container>
   );
 };
 
