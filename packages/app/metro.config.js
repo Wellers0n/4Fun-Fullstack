6cecaea17a39eb6c27283041297e0cf6c3d32f64
Wellerson <wellerson.coffee@gmail.com>
2019-06-30 20:37:55 -0300

    new config metro.config.js :rocket:

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -6,9 +6,29 @@
  */
 
 const path = require('path');
+const getWorkspaces = require('get-yarn-workspaces');
+const workspaces = getWorkspaces(__dirname);
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 module.exports = {
-  watchFolders: [path.resolve(__dirname, './node_modules')],
+   projectRoot: path.resolve(__dirname, '.'),
+  // watchFolders: [path.resolve(__dirname, './node_modules')],
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    ...workspaces,
+  ],
+
+  resolver: {
+        blacklistRE: blacklist([
+            /node_modules\/.*\/node_modules\/react-native\/.*/,
+        ]),
+
+        // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+        extraNodeModules: new Proxy({}, {
+          get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+        }),
+    },
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
